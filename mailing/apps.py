16864013eda88d2cc1656928554f539e9890910b
ElizavetaAveryanova <153016425+ElizavetaAveryanova@users.noreply.github.com>
2024-06-01 02:20:11 +0500

    work_change_

--- mailing/apps.py
+++ mailing/apps.py
@@ -6,7 +6,7 @@ class MailingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mailing'
 
-    # def ready(self):
-    #     from scheduler import start
-    #     time.sleep(2)
-    #     start()
+    def ready(self):
+        from scheduler import start
+        time.sleep(2)
+        start()
--- users/views.py
+++ users/views.py
@@ -6,8 +6,9 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views import View
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, FormView
 from config import settings
+from config.settings import DEFAULT_FROM_EMAIL
 from users.forms import RegisterCreationForm, UserProfileForm, PasswordResetForm
 from users.models import User
 
@@ -51,33 +52,58 @@ class ProfileView(UpdateView):
     def get_object(self, queryset=None):
         return self.request.user
 
-class PasswordResetView(View):
-    template_name = 'users/password_reset_form.html'
-    form_class = PasswordResetForm
-    success_url = reverse_lazy('users:login')
+# class PasswordResetView(View):
+#     template_name = 'users/password_reset_form.html'
+#     form_class = PasswordResetForm
+#     success_url = reverse_lazy('users:login')
+#
+#     def get(self, request):
+#         form = self.form_class()
+#         return render(request, self.template_name, {'form': form})
+#
+#     def post(self, request):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             email = form.cleaned_data['email']
+#             user = get_object_or_404(User, email=email)
+#             letters = list(string.ascii_lowercase)
+#             new_password = ''
+#             for _ in range(5):
+#                 new_password = new_password + random.choice(letters) + str(random.randint(1, 9))
+#             user.set_password(new_password)
+#             user.save()
+#             message = f"Ваш новый пароль: {new_password}\nНикому его не сообщайте."
+#             send_mail(
+#                 'Новый пароль',
+#                 message,
+#                 settings.EMAIL_HOST_USER,
+#                 [user.email],
+#                 fail_silently=False,
+#             )
+#             return redirect(self.success_url)
+#         return render(request, self.template_name, {'form': form})
 
-    def get(self, request):
-        form = self.form_class()
-        return render(request, self.template_name, {'form': form})
 
-    def post(self, request):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            email = form.cleaned_data['email']
-            user = get_object_or_404(User, email=email)
-            letters = list(string.ascii_lowercase)
-            new_password = ''
-            for _ in range(5):
-                new_password = new_password + random.choice(letters) + str(random.randint(1, 9))
-            user.set_password(new_password)
-            user.save()
-            message = f"Ваш новый пароль: {new_password}\nНикому его не сообщайте."
-            send_mail(
-                'Новый пароль',
-                message,
-                settings.EMAIL_HOST_USER,
-                [user.email],
-                fail_silently=False,
-            )
-            return redirect(self.success_url)
-        return render(request, self.template_name, {'form': form})
+class PasswordResetView(FormView):
+    model = User
+    template_name = "users/password_reset_form.html"
+    success_url = reverse_lazy("users:login")
+
+    def form_valid(self, form):
+        email_form = form.cleaned_data("email")
+        user = User.objects.get(email=email_form)
+
+        letters = list(string.ascii_lowercase)
+        new_password = ''
+        for _ in range(5):
+            new_password = new_password + random.choice(letters) + str(random.randint(1, 9))
+
+        user.set_password(new_password)
+        user.save()
+        send_mail(
+            subject="Новый пароль",
+            message=f"Ваш пароль: {new_password}",
+            from_email=DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+        )
+        return super().form_valid(form)
